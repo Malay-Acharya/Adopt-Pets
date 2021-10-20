3d96692e47e54ff6dda46baf2d95fb72d23e0833
Malay Acharya <malayacharya21@gmail.com>
2021-10-20 18:18:28 +0530

    Change boilerplate react to jsx and added eslint support for react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "A", animal: "B", breed: "C" }),
-    React.createElement(Pet, { name: "A", animal: "B", breed: "C" }),
-    React.createElement(Pet, { name: "A", animal: "BC", breed: "CD" }),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, { name: "A", animal: "B", breed: "C" }),
+//     React.createElement(Pet, { name: "A", animal: "B", breed: "C" }),
+//     React.createElement(Pet, { name: "A", animal: "BC", breed: "CD" }),
+//   ]);
+// };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+const App = () =>{
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="A" animal="B" breed="C"/>
+      <Pet name="A" animal="B" breed="C"/>
+      <Pet name="A" animal="BC" breed="CD"/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
