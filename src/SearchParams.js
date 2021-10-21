5c013a4b0bb70b82b7b5297e306aaf159e8d0e9e
Malay Acharya <malayacharya21@gmail.com>
2021-10-21 19:28:29 +0530

    Added the UseSTATE hook to breed and animals

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["dog", "bird", "cat", "squirrel"];
+const BREEDS = ["Labrador", "b breed", "c breed"];
+
 const SearchParams = () => {
   const [location, setlocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("Labrador");
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,42 @@ const SearchParams = () => {
             value={location}
             placeholder="location"
           />
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => {
+              return (
+                <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option></option>
+            {BREEDS.map((breed) => {
+              return (
+                <option value={breed} key={breed}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
